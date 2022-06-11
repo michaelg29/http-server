a331fd82886e4654c383eb111a13e1a65f4bec04
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-06-11 15:30:26 -0400

    Add test function

---
+++ http-server/HttpServer.Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using HttpServer;
+using HttpServer.Logging;
+using System;
+
+namespace HttpServer.Test
+{
+    enum TestEnum
+    {
+        TestEnum1 = 0,
+        TestEnum2 = 1
+    }
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Message m = new Message("Title");
+            m.With("value")
+                .With(new ArithmeticException("test arithmetic"))
+                .With(TestEnum.TestEnum1)
+                .With("header1", "value1");
+            Console.WriteLine(m.ToString());
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
