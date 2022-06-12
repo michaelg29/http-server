ae1bdd4eacd7c5189c1a455effb206415bdaec48
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-06-12 10:38:52 -0400

    Create public server interface

--- http-server/HttpServer/IHttpServer.cs
+++ http-server/HttpServer/IHttpServer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,19 @@ namespace HttpServer
 {
     public interface IHttpServer
     {
+        HttpStatusCode ResponseCode
+        {
+            set;
+        }
+        long ContentLength
+        {
+            set;
+        }
+        string ContentType
+        {
+            set;
+        }
+        Task SendFileAsync(string filePath, params string[] args);
+        Task<int> RunAsync(string[] args);
     }
 }
