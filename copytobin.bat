eafc944efb0640f634efaedea696a2a6596e25be
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-06-12 12:27:50 -0400

    Add release files

---
+++ copytobin.bat
@@ -0,0 +1,5 @@
+copy http-server\HttpServer\bin\Release\HttpServer.dll bin
+copy http-server\StaticFileServer\bin\Release\StaticFileServer.exe bin
+copy http-server\StaticFileServer\bin\Release\directory.html bin
+copy http-server\StaticFileServer\bin\Release\error.html bin
+copy http-server\StaticFileServer\bin\Release\notFound.html bin
