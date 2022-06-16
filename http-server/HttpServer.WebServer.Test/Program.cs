77fba4fc8a4efa79c0321ae2e31d44c62c635249
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-06-15 23:15:35 -0400

    Create Program.cs

---
+++ http-server/HttpServer.WebServer.Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using HttpServer.WebServer;
+
+namespace HttpServer.WebServer.Test
+{
+    class Program
+    {
+        public static void Action_(WebServer ws, params object[] args) {}
+        public static void Action_Hello(WebServer ws, params object[] args) { }
+        public static void Action_int(WebServer ws, params object[] args) { }
+        public static void Action_Hello_test_int_str(WebServer ws, params object[] args) { }
+
+        static void Main(string[] args)
+        {
+            RouteTree rt = new RouteTree();
+            rt.AddRoute("/", Action_);
+            rt.AddRoute("/hello", Action_Hello);
+            rt.AddRoute("/{int}", Action_int);
+            rt.AddRoute("/Hello/test/{int}/{str}", Action_Hello_test_int_str);
+        }
+    }
+}
