341812ab92877e411ef6a77687f0d583c3b77656
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-06-15 23:40:17 -0400

    Test routes

--- http-server/HttpServer.WebServer.Test/Program.cs
+++ http-server/HttpServer.WebServer.Test/Program.cs
@@ -6,18 +6,39 @@ namespace HttpServer.WebServer.Test
 {
     class Program
     {
-        public static void Action_(WebServer ws, params object[] args) {}
-        public static void Action_Hello(WebServer ws, params object[] args) { }
-        public static void Action_int(WebServer ws, params object[] args) { }
-        public static void Action_Hello_test_int_str(WebServer ws, params object[] args) { }
+        public static void Action_(WebServer ws)
+        {
+            Console.WriteLine("Action_");
+        }
+
+        public static void Action_Hello(WebServer ws)
+        {
+            Console.WriteLine("Action_Hello");
+        }
+        public static void Action_int(WebServer ws, int arg0)
+        {
+            Console.WriteLine($"Action_int: {arg0}");
+        }
+
+        public static void Action_hello_test_int_str(WebServer ws, int arg0, string arg1)
+        {
+            Console.WriteLine($"Action_hello_test_int_str: {arg0}, {arg1}");
+        }
 
         static void Main(string[] args)
         {
             RouteTree rt = new RouteTree();
-            rt.AddRoute("/", Action_);
-            rt.AddRoute("/hello", Action_Hello);
-            rt.AddRoute("/{int}", Action_int);
-            rt.AddRoute("/Hello/test/{int}/{str}", Action_Hello_test_int_str);
+            rt.AddRoute("/", (ws, args) => Action_(ws));
+            rt.AddRoute("/hello", (ws, args) => Action_Hello(ws));
+            rt.AddRoute("/{int}", (ws, args) => Action_int(ws, (int)args[0]));
+            rt.AddRoute("/hello/test/{int}/{str}", (ws, args) => Action_hello_test_int_str(ws, (int)args[0], (string)args[1]));
+
+            Console.WriteLine(rt.Navigate(null, "/"));
+            Console.WriteLine(rt.Navigate(null, "/hello"));
+            Console.WriteLine(rt.Navigate(null, "/5"));
+            Console.WriteLine(rt.Navigate(null, "/123"));
+            Console.WriteLine(rt.Navigate(null, "/123.5"));
+            Console.WriteLine(rt.Navigate(null, "/hello/test/15/adls;kfj"));
         }
     }
 }
