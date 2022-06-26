8a64ea7fc22da274f693e7ff202159897db3dbb0
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-06-26 08:39:09 -0400

    Add documentation

--- HttpServer/HttpServer.Main/Extensions.cs
+++ HttpServer/HttpServer.Main/Extensions.cs
@@ -2,8 +2,14 @@
 
 namespace HttpServer.Main
 {
+    /// <summary>
+    /// Extensions needed for the HttpServer instances
+    /// </summary>
     public static class Extensions
     {
+        /// <summary>
+        /// Patch HttpMethod
+        /// </summary>
         public static HttpMethod Patch { get; } = new HttpMethod("PATCH");
     }
 }
