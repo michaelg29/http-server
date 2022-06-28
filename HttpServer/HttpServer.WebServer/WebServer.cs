9d6d3f5c76b804054c5507c7c24751525be69019
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-06-27 21:46:36 -0400

    Create web server class

---
+++ HttpServer/HttpServer.WebServer/WebServer.cs
@@ -0,0 +1,30 @@
+﻿using HttpServer.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer.WebServer
+{
+    public class WebServer : Main.HttpServer
+    {
+        /// <inheritdoc />
+        public WebServer(string hostUrl = null, string hostDir = null, ILogger logger = null)
+            : base(hostUrl, hostDir, logger) { }
+
+        /// <summary>
+        /// Process current request
+        /// </summary>
+        protected override async Task ProcessRequest()
+        {
+            
+        }
+
+        /// <inheritdoc />
+        protected override async Task<string> GetCommand()
+        {
+            return await Task.FromResult(Console.ReadLine());
+        }
+    }
+}
