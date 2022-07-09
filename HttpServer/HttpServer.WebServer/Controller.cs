21db7dd521bf480d6d7fb0ca345675881c9d66ed
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-07-09 14:02:47 -0400

    Create controller class

---
+++ HttpServer/HttpServer.WebServer/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpServer.WebServer
+{
+    public abstract class Controller
+    {
+        internal IHttpServer _server { get; set; }
+
+        protected void Shutdown()
+        {
+            _server.Shutdown();
+        }
+
+        protected HttpListenerRequest Request
+        {
+            get => _server.Request;
+        }
+
+        public void SetResponse(HttpStatusCode responseCode = 0,
+            string contentType = null,
+            Encoding encoding = null)
+        {
+            if (responseCode != 0)
+            {
+                _server.ResponseCode = responseCode;
+            }
+            if (!string.IsNullOrEmpty(contentType))
+            {
+                _server.ContentType = contentType;
+            }
+            if (encoding != null)
+            {
+                _server.Encoding = encoding;
+            }
+        }
+    }
+}
