7ba32f10f87c1fc5ae68d6e3d1dee03a976faf1e
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-07-15 00:38:33 -0400

    View documentation

--- HttpServer/HttpServer.WebServer/View.cs
+++ HttpServer/HttpServer.WebServer/View.cs
@@ -7,14 +7,31 @@ using System.Threading.Tasks;
 
 namespace HttpServer.WebServer
 {
+    /// <summary>
+    /// Class representing a file to be sent as a response.
+    /// </summary>
     public class View
     {
+        /// <summary>
+        /// Path to the file.
+        /// </summary>
         public string Filepath { get; set; }
 
+        /// <summary>
+        /// Mime type of the view.
+        /// </summary>
         public string MimeType { get; set; }
 
+        /// <summary>
+        /// Formatting arguments.
+        /// </summary>
         public string[] Args { get; set; }
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="filepath">Filepath</param>
+        /// <param name="args">Formatting arguments</param>
         public View(string filepath, params object[] args)
         {
             Filepath = filepath;
@@ -22,6 +39,10 @@ namespace HttpServer.WebServer
             Args = args.Select(arg => arg.ToString()).ToArray();
         }
 
+        /// <summary>
+        /// Execute the view by reading the file and applying the arguments
+        /// </summary>
+        /// <returns>Formatted string</returns>
         public string Format()
         {
             string content = null;
