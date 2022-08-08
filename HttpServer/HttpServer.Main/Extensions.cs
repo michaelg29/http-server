80742e96b41959ec7be30dd238537cef6ce4f4a9
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2022-08-07 20:24:21 -0400

    Create byte list extension methods

--- HttpServer/HttpServer.Main/Extensions.cs
+++ HttpServer/HttpServer.Main/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 
 namespace HttpServer.Main
 {
@@ -20,5 +21,50 @@ namespace HttpServer.Main
         public const string HttpOptions = "OPTIONS";
         public const string HttpTrace = "TRACE";
         public const string HttpPatch = "PATCH";
+
+        public static bool TryIndexOf(this IList<byte> buffer, int len, byte[] target, out int idx, int startIdx = 0)
+        {
+            idx = buffer.IndexOf(len, target, startIdx);
+            return idx != -1;
+        }
+
+        public static int IndexOf(this IList<byte> buffer, int len, byte[] target, int startIdx = 0)
+        {
+            int upperBound = len - target.Length + startIdx;
+            for (int i = startIdx; i < upperBound; i++)
+            {
+                int skip = 0;
+                bool canSkip = false;
+                bool match = true;
+                for (int j = 0; j < target.Length && match; j++)
+                {
+                    // do not re-check first characters that do not match the first byte in the target
+                    if (canSkip && buffer[i + j] != target[0])
+                    {
+                        skip++;
+                    }
+                    else
+                    {
+                        canSkip = false;
+                    }
+
+                    if (j == 0)
+                    {
+                        canSkip = true;
+                    }
+
+                    match = buffer[i + j] == target[j];
+                }
+
+                if (match)
+                {
+                    return i;
+                }
+
+                i += skip;
+            }
+
+            return -1;
+        }
     }
 }
