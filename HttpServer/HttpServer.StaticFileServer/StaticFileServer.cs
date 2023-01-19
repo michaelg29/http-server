20cad01f425e8ca79f8d50437ff8235b43b4868c
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2023-01-19 11:41:26 -0500

    Create upload form for static file server

--- HttpServer/HttpServer.StaticFileServer/StaticFileServer.cs
+++ HttpServer/HttpServer.StaticFileServer/StaticFileServer.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace HttpServer.StaticFileServer
 {
@@ -21,13 +22,31 @@ namespace HttpServer.StaticFileServer
         protected string errorPath = "error.html";
         protected string notFoundPath = "notFound.html";
         protected string dirPath = "directory.html";
+        protected string uploadPath = "upload.html";
+
+        protected string uploadDirectory = "uploadbin";
+        protected string uploadRequestRoute;
 
         /// <inheritdoc />
         public StaticFileServer(string hostUrl = null, string hostDir = null, ILogger logger = null)
             : base(hostUrl, hostDir, logger) { }
 
         /// <inheritdoc />
-        protected override async Task Startup() { }
+        protected override async Task Startup()
+        {
+            // create upload directory
+            Directory.CreateDirectory(GetTemplatePath(uploadDirectory));
+
+            // amend upload request path if needed
+            uploadRequestRoute = "/upload";
+            int i = 0;
+            while (Directory.Exists(GetTemplatePath(uploadRequestRoute)))
+            {
+                uploadRequestRoute = $"/upload{i}";
+                ++i;
+            }
+            logger.Send($"Access the upload form at the route {uploadRequestRoute}");
+        }
 
         /// <summary>
         /// Process current request
@@ -44,15 +63,15 @@ namespace HttpServer.StaticFileServer
                 return;
             }
 
-            // get absolute path for file access
-            string absolutePath = AbsolutePath(route);
-            if (absolutePath.Contains(".."))
-            {
-                absolutePath = null;
-            }
-
             try
             {
+                // get absolute path for file access
+                string absolutePath = AbsolutePath(route);
+                if (absolutePath.Contains(".."))
+                {
+                    absolutePath = null;
+                }
+
                 if (absolutePath != null && File.Exists(absolutePath))
                 {
                     // physical file exists
@@ -97,7 +116,48 @@ namespace HttpServer.StaticFileServer
 
                     // send formatted file
                     await _SendFileAsync(GetTemplatePath(dirPath),
-                        route, directoryList, fileList);
+                        route, directoryList, fileList, uploadRequestRoute);
+                }
+                else if (route == uploadRequestRoute)
+                {
+                    if (ctx.Request.HttpMethod == "POST")
+                    {
+                        // receive upload file
+                        var bodyStream = ctx.Request.InputStream;
+                        var filenames = ctx.Request.QueryString.GetValues("name");
+                        var filename = filenames != null && filenames.Length > 0
+                            ? filenames[0]
+                            : $"upload-{DateTime.Now}";
+
+                        // read as buffer
+                        List<byte> bytes = new List<byte>();
+                        byte[] buffer = new byte[1024];
+                        int noBytes;
+                        while ((noBytes = bodyStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            int diff = buffer.Length - noBytes;
+                            bytes.AddRange(buffer);
+                            if (diff > 0)
+                            {
+                                bytes.RemoveRange(bytes.Count - diff, diff);
+                            }
+                        }
+                        var bodyBuffer = bytes.ToArray();
+
+                        // write file to output directory
+                        FileStream f = File.OpenWrite($"{uploadDirectory}/{filename}");
+                        f.Write(bodyBuffer, 0, bodyBuffer.Length);
+                        f.Close();
+
+                        logger.Send(new Message("Downloaded file")
+                            .With("length", bodyBuffer.Length)
+                            .With("name", filename));
+                    }
+                    else
+                    {
+                        // return upload form
+                        await _SendFileAsync(GetTemplatePath(uploadPath), uploadRequestRoute);
+                    }
                 }
                 else
                 {
