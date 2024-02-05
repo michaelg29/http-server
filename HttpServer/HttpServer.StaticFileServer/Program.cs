7c5c5fc05cb2a4f3ffc12d83c49213f652bccaf9
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2024-02-05 10:49:56 -0500

    Improve command line options

--- HttpServer/HttpServer.StaticFileServer/Program.cs
+++ HttpServer/HttpServer.StaticFileServer/Program.cs
@@ -13,9 +13,11 @@ namespace HttpServer.StaticFileServer
         {
             Console.WriteLine("Hello, world!");
 
-            string hostUrl = "http://+:8080/;https://+:8443";
-            string hostDir = string.Empty;
+            bool https = false;
+            bool local = false;
             bool allowUpload = false;
+            string hostDir = string.Empty;
+            string passcode = string.Empty;
 
             for (int i = 0; i < args.Length; i++)
             {
@@ -23,24 +25,47 @@ namespace HttpServer.StaticFileServer
                 {
                     // valued argument
                     string value = args[i + 1];
-                    if (args[i] == "-u")
-                    {
-                        hostUrl = value.EndsWith("/")
-                            ? value
-                            : value + "/";
-                    }
-                    else if (args[i] == "-d")
+                    if (args[i] == "-d" || args[i] == "--dir")
                     {
                         hostDir = value;
                     }
+                    else if (args[i] == "-p" || args[i] == "--pass")
+                    {
+                        https = true;
+                        passcode = value;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+
+                    i += 1;
                 }
 
-                if (args[i] == "-p")
+                if (args[i] == "-s" || args[i] == "-https")
+                {
+                    https = true;
+                }
+                else if (args[i] == "-l" || args[i] == "--local")
+                {
+                    local = true;
+                }
+                else if (args[i] == "-u" || args[i] == "--upload")
                 {
                     allowUpload = true;
                 }
             }
 
+            // construct URL
+            string ip = local ? "localhost" : "+";
+            string hostUrl = $"https://{ip}:8443/";
+            if (!https)
+            {
+                hostUrl += $";http://{ip}:8080/";
+            }
+            Console.WriteLine($"Hosting at address {hostUrl}");
+
+            // run server
             StaticFileServer server = new StaticFileServer(hostUrl, hostDir, allowUpload, Logger.ConsoleLogger);
             int res = await server.RunAsync();
 
